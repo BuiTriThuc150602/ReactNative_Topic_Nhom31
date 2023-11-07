0101f4cfc8304fb6901364eaf85deb96d28c1495
BuiTriThuc150602 <20088361.thuc@student.iuh.edu.vn>
2023-11-07 20:38:30 +0700

    Complete Login UI

--- screen/Login.js
+++ screen/Login.js
@@ -1,20 +1,44 @@
-import { StyleSheet, Text, View, Image, TextInput } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  ImageBackground,
+  Pressable,
+} from "react-native";
 import React from "react";
 
 const Login = () => {
   return (
     <View style={styles.container}>
-      <View style={styles.backGround}>
-        <Image
-          source={require("../img/background_login.png")}
-          style={styles.logo}
-        />
-        <Text style={styles.title}>An app made for learning</Text>
-      </View>
-      <View style={styles.formContainer}>
-        <TextInput placeholder="username" style={styles.input} />
-        <TextInput placeholder="password" style={styles.input} />
-      </View>
+      <ImageBackground
+        source={require("../img/background_login.png")}
+        style={styles.background}
+        resizeMode="cover"
+      >
+        <View style={styles.formContainer}>
+          <TextInput placeholder="Email" style={styles.input} />
+          <TextInput placeholder="Password" style={styles.input} />
+          <View style={styles.linksRow}>
+            <Pressable>
+              <Text style={styles.links}>Sign Up</Text>
+            </Pressable>
+            <Pressable>
+              <Text style={styles.links}>Forgot Password</Text>
+            </Pressable>
+          </View>
+          <Pressable style={styles.btnLogin}>
+            <Text style={styles.Login}>Sign In</Text>
+          </Pressable>
+        </View>
+        <Text style={{ color: "white", marginTop: 50 }}>Sign in with</Text>
+        <View style={styles.moreLogin}>
+          <Image source={require("../img/gg-logo.png")} resizeMode="contain" style={styles.logo_icons} />
+          <Image source={require("../img/fb-logo.png")}  resizeMode="contain" style={styles.logo_icons}/>
+          <Image source={require("../img/phone-logo.png")} resizeMode="contain" style={styles.logo_icons} />
+        </View>
+      </ImageBackground>
     </View>
   );
 };
@@ -22,33 +46,58 @@ const Login = () => {
 export default Login;
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: "#3498db",
-        flex: 1,
-    },
-    backGround: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-    },
-    logo: {
-        width: 200,
-        height: 200,
-    },
-    title: {
-        fontSize: 20,
-        color: "#fff",
-        marginTop: 10,
-    },
-    formContainer: {
-        flex: 1,
-        padding: 20,
-    },
-    input: {
-        backgroundColor: "#fff",
-        marginBottom: 10,
-        borderRadius: 5,
-        padding: 10,
-    },
+  container: {
+    flex: 1,
+  },
+  background: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  formContainer: {
+    width: "90%",
+    backgroundColor: "none",
+  },
+  input: {
+    backgroundColor: "none",
+    marginBottom: 20,
+    paddingVertical: 15,
+    borderBottomWidth: 1,
+    borderBottomColor: "white",
+    color: "white",
+  },
+  links: {
+    color: "red",
+    marginBottom: 20,
+    fontWeight: "100",
+    borderBottomWidth: 1,
+    borderBottomColor: "red",
+  },
+  linksRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  btnLogin: {
+    backgroundColor: "#54D933",
+    paddingVertical: 10,
+    borderRadius: 15,
+    marginTop: 20,
+    marginHorizontal: 80,
+    alignItems: "center",
+  },
+  Login: {
+    color: "white",
+    fontWeight: "700",
+    fontSize: 24,
+  },
+  moreLogin: {
+    flexDirection: "row",
+    marginTop: 80
+  },
+  logo_icons: {
+    width: 60,
+    height: 60,
+    marginHorizontal: 10,
+  },
 
 });
